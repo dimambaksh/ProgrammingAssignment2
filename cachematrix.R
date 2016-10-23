321fc187115890cca60fc54a2346817e74bbb389
dimambaksh <dimambaksh@gmail.com>
2016-10-23 10:13:26 -0500

    Added test.R used to review submissions
    While reviewing assignment submissions, test.R is used to review the code.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,14 @@
 ## cacheSolve accepts a matrix cache function and attempts to return the cached or computed inverse of the matrix 
 ## Examples:
 ## 10x10 Matrix initialized randomly without repeating values
-## mtrx <- matrix(sample(c(1:100), size = 100, replace = TRUE), nrow=10, ncol=10)
+## mtrx <- matrix(sample(c(1:100), size = 100, replace = FALSE), nrow=10, ncol=10)
 ## mtrxobject <- makeCacheMatrix(mtrx)
 ## cacheSolve(mtrxobject)
 ## mtrxobject$get() #retrieves the original matrix
 ## mtrxobject$getmatrix() #retrieves the cached inverse of the matrix
 ##
 ## 4x4 matrix initialized randomly without repeating values, setting a new 4x4 matrix to be cached
-## mtrx2 <- matrix(sample(c(1:16),16,replace=TRUE), nrow=4, ncol=4)
+## mtrx2 <- matrix(sample(c(1:16),16,replace=FALSE), nrow=4, ncol=4)
 ## mtrxobject$set(mtrx2)  #sets the NEW non-inverse matrix
 ## mtrxobject$get()       #retrieves the non-inverse matrix
 ## mtrxobject$getmatrix() #Will return a null because a new matrix was set and the inverse has not been cached
